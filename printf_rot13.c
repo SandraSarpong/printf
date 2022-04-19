d37085b4ae0cb999567a33b1e3206182e7d31c7c
Sandra Sarpong <90385305+SandraSarpong@users.noreply.github.com>
2022-04-19 23:28:09 +0000

    prints the rot13'ed string

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,44 +1,38 @@
 #include "main.h"
+
 /**
- * printf_unsigned - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ * printf_rot13 - printf str to ROT13 place into buffer
+ * @args: type struct va_arg where is allocated printf arguments
+ * Return: counter
+ *
  */
-int printf_unsigned(va_list args)
+int printf_rot13(va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
 	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
 		{
-			exp = exp * 10;
-			num = num / 10;
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
 		}
-		num = n;
-		while (exp > 0)
+		if (!k)
 		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
+			_putchar(s[i]);
+			counter++;
 		}
 	}
-	_putchar(last + '0');
-
-	return (i);
+	return (counter);
 }
