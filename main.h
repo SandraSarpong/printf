8b376add1c2b5aa3152c0ce0aead32b6439b9589
SandraSarpong <maameyaasarp@gmail.com>
2022-04-21 20:19:14 -0700

    using printf to print outputs

--- main.h
+++ main.h
@@ -67,4 +67,10 @@ int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
 
-/* last 5 lines */
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
+/* last line */
